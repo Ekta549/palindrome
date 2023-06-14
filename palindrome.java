8c06ae4697353b70a7e727ded17e1603d7547f97
Ekta549 <133002329+Ekta549@users.noreply.github.com>
2023-06-14 14:55:58 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,15 +1,15 @@
 public class palindrome {
         public static void main(String[] args) {
       
-          String str = "Mom", reverseStr = "";
+          String str = "Mom", revStr = "";
           
           int strLength = str.length();
       
           for (int i = (strLength - 1); i >=0; --i) {
-            reverseStr = reverseStr + str.charAt(i);
+            revStr = revStr + str.charAt(i);
           }
       
-          if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+          if (str.toLowerCase().equals(revStr.toLowerCase())) {
             System.out.println(str + " is a Palindrome String.");
           }
           else {
