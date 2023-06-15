f7b5fe30fd28b126fc6498169a2161374234862f
Ekta549 <133002329+Ekta549@users.noreply.github.com>
2023-06-15 11:54:51 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,20 +1,24 @@
+import java.util.Scanner;
+
 public class palindrome {
         public static void main(String[] args) {
       
-          String str = "Mom", revStr = "";
-          
+          String str, revStr = "";
+      Scanner sc = new Scanner(System.in);
+ 
+      System.out.println("Enter a string:");
+      str = sc.nextLine();
           int strLength = str.length();
       
           for (int i = (strLength - 1); i >=0; --i) {
             revStr = revStr + str.charAt(i);
           }
       
-          if (str.toLowerCase().equals(revStr.toLowerCase())) {
-            System.out.println(str + " is a Palindrome String.");
+           if (str.equals(revStr))
+         System.out.println(str+" is a palindrome");
+      else
+         System.out.println(str+" is not a palindrome");
+ 
+   }
           }
-          else {
-            System.out.println(str + " is not a Palindrome String.");
-          }
-        }
-      
-}
+        
